da340c6a272ed77b30e61ba9bf2d9f5ccfee67dc
ammsinn <kim1109123@gmail.com>
2025-02-24 22:45:12 +0900

    plasmo init

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [data, setData] = useState("");
 
   return (
     <div
       style={{
         display: "flex",
         flexDirection: "column",
-        padding: 16
+        padding: 16 
       }}>
       <h1>
         Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
@@ -16,7 +16,7 @@ function IndexPopup() {
       <input onChange={(e) => setData(e.target.value)} value={data} />
       <footer>Crafted by @PlasmoHQ</footer>
     </div>
-  )
-}
+  );
+};
 
-export default IndexPopup
+export default IndexPopup;
