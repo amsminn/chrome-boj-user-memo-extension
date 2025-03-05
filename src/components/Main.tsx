619f82a427aa3274fb807bc537ea93a57e7c3e64
ammsinn <kim1109123@gmail.com>
2025-03-05 20:10:29 +0900

    permission, dependency upd

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,10 +3,42 @@ import Container from "./Container";
 import MemoInput from "./MemoInput";
 import UpdateButton from "./UpdateButton";
 import GuideButton from "./GuideButton";
-import useFetchMemo from "../hooks/useFetchMemo";
+import useUrl from "../hooks/useUrl";
+import { useContext, useEffect } from "react";
+import MemoContext from "../Context";
+import { useStorage } from "@plasmohq/storage/hook";
+import { Storage } from "@plasmohq/storage";
 
 const Main: React.FC = ()=> {
-  useFetchMemo();
+  const url = useUrl();
+  const { flag, handle, text, setFlag, setHandle, setText } = useContext(MemoContext);
+
+  useEffect(() => {
+    console.log("URL changed to: " + url);
+    if (url.includes("acmicpc.net/user/")) {
+      setFlag(true);
+      const parts = url.split("/");
+      const newHandle = parts.at(-1) || "";
+      setHandle(newHandle);
+    } else {
+      setFlag(false);
+      setHandle("");
+      setText("");
+    }
+  }, [url, setFlag, setHandle, setText]);
+
+  const storageKey = handle ? `Boj_${handle}` : "BojMemo";
+  const [memo] = useStorage<string>({
+    key: storageKey,
+    instance: new Storage({ area: "sync" })
+  });
+
+  useEffect(() => {
+    if (handle) {
+      setText(memo);
+      console.log(`Fetched memo for ${handle}: ${memo}`);
+    }
+  }, [handle, memo, setText]);
 
   return (
     <Container>
